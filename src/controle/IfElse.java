73530f20b857658ba2abba0266793bbd1bd8d25f
VitorLima <vitorgl@ciandt.com>
2022-07-19 21:31:15 -0300

    If else

---
+++ src/controle/IfElse.java
@@ -0,0 +1,21 @@
+package controle;
+
+import javax.swing.*;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        String valor = JOptionPane.showInputDialog(
+                "Informe um número: ");
+        int numero = Integer.parseInt(valor);
+
+        if(numero % 2 == 0){
+            System.out.println("Número par!");
+        } else
+            System.out.println("Número ímpar!");
+
+    }
+
+
+}
+
